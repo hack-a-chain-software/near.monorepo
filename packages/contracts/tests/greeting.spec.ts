8462a5de546843c4440bbcede5c19f024ad3adbc
LFSCamargo <luizepauloxd@gmail.com>
2022-05-13 14:51:04 -0300

    chore: cleanup duplicated code

--- packages/contracts/tests/greeting.spec.ts
+++ packages/contracts/tests/greeting.spec.ts
@@ -2,37 +2,7 @@ import "jest";
 import { Account, connect, Contract } from "near-api-js";
 import { KeyStore } from "near-api-js/lib/key_stores";
 import { NearConfig } from "near-api-js/lib/near";
-
-/**
- * @name GreetingContract
- * @description - This will handle the interactions
- * with the contract with better TS types
- */
-export class GreetingContract {
-  private contract: any;
-
-  /**
-   * @constructor
-   * @param contract
-   */
-  constructor(contract: any) {
-    this.contract = contract as any;
-  }
-
-  /**
-   * @description - This method gets the message on chain to the user account_id
-   */
-  public async getGreeting(params: { account_id: string }): Promise<string> {
-    return await this.contract.get_greeting(params);
-  }
-
-  /**
-   * @description - This method updates the message for the account on NEAR
-   */
-  public async updateGreeting(params: { message: string }) {
-    return await this.contract.set_greeting(params, "300000000000000");
-  }
-}
+import { GreetingContract } from "../lib";
 
 describe("Greeting Contract Tests", () => {
   let contract: GreetingContract;
